8419040ae0479863b680583a5b5a3919f5592c34
bom2013 <noam21288@gmail.com>
2020-07-19 00:18:23 +0300

    Finish

--- PE parser/PE parser.cpp
+++ PE parser/PE parser.cpp
@@ -1,3 +1,9 @@
+/*
+Auther: Noam Ben Shlomo(Bom2013)
+This code is written by a code written by Uri Haddad,
+the original code can be found at the following link: https://drive.google.com/file/d/1jxf3JU0qv8wdsscpB9gW5_raDhmy2q0u/view
+credit to him(Uri haddad)
+*/
 #include <iostream>
 #include <Windows.h>
 
@@ -42,9 +48,9 @@ LPVOID getHandleToMappedFile(char* path)
 /// <param name="printImageFileHeaderMetadata">Pointer to the image file header</param>  
 void printImageFileHeaderMetadata(PIMAGE_FILE_HEADER ptrImageFileHeader)
 {
-	cout << "FileHeader.Machine: " << ptrImageFileHeader->Machine << endl;
-	cout << "FileHeader.TimeDateStamp: " << ptrImageFileHeader->TimeDateStamp << endl;
-	cout << "FileHeader.NumberOfSections: " << ptrImageFileHeader->NumberOfSections << endl;
+	cout << "\tFileHeader.Machine: " << ptrImageFileHeader->Machine << endl;
+	cout << "\tFileHeader.TimeDateStamp: " << ptrImageFileHeader->TimeDateStamp << endl;
+	cout << "\tFileHeader.NumberOfSections: " << ptrImageFileHeader->NumberOfSections << endl;
 
 }
 
@@ -52,12 +58,53 @@ void printImageFileHeaderMetadata(PIMAGE_FILE_HEADER ptrImageFileHeader)
 /// <param name="ptrImageOptionalHeader">Pointer to the imageOptionHeader</param>  
 void printImageOptionalHeaderMetadata(PIMAGE_OPTIONAL_HEADER ptrImageOptionalHeader)
 {
-	cout << "OptionalHeader.Magic: " << ptrImageOptionalHeader->Magic << endl;
-	cout << "OptionalHeader.AddressOfEntryPoint: " << ptrImageOptionalHeader->AddressOfEntryPoint << endl;
-	cout << "OptionalHeader.SizeOfImage: " << ptrImageOptionalHeader->SizeOfImage << endl;
-	cout << "OptionalHeader.SectionAlignment: " << ptrImageOptionalHeader->SectionAlignment << endl;
-	cout << "OptionalHeader.FileAlignment: " << ptrImageOptionalHeader->FileAlignment << endl;
-	cout << "OptionalHeader.ImageBase: " << ptrImageOptionalHeader->ImageBase << endl;
+	cout << "\tOptionalHeader.Magic: " << ptrImageOptionalHeader->Magic << endl;
+	cout << "\tOptionalHeader.AddressOfEntryPoint: " << ptrImageOptionalHeader->AddressOfEntryPoint << endl;
+	cout << "\tOptionalHeader.SizeOfImage: " << ptrImageOptionalHeader->SizeOfImage << endl;
+	cout << "\tOptionalHeader.SectionAlignment: " << ptrImageOptionalHeader->SectionAlignment << endl;
+	cout << "\tOptionalHeader.FileAlignment: " << ptrImageOptionalHeader->FileAlignment << endl;
+	cout << "\tOptionalHeader.ImageBase: " << ptrImageOptionalHeader->ImageBase << endl;
+}
+
+void printExportMetaData(PIMAGE_EXPORT_DIRECTORY ptrImageExportDirectory)
+{
+	cout << "Exports:" << endl;
+	cout << "\tExports.Name: " << ptrImageExportDirectory->Name << endl;
+	cout << "\tExports.Base: " << ptrImageExportDirectory->Base << endl;
+	cout << "\tExports.NumberOfFunctions: " << ptrImageExportDirectory->NumberOfFunctions << endl;
+	cout << "\tExports.NumberOfNames: " << ptrImageExportDirectory->NumberOfNames << endl;
+}
+void printExportFunctionsData(DWORD imageBase, PIMAGE_EXPORT_DIRECTORY ptrImageExportDirectory, DWORD exportStartRVA, DWORD exportEndRVA)
+{
+	//Get pointers to arrays
+	PDWORD ptrExportsFunctions = (PDWORD)(imageBase + ptrImageExportDirectory->AddressOfFunctions);
+	PDWORD ptrExportsNames = (PDWORD)(imageBase + ptrImageExportDirectory->AddressOfNames);
+	PWORD ptrExportsNameOrdinals = (PWORD)(imageBase + ptrImageExportDirectory->AddressOfNameOrdinals);
+
+	//print functions
+	cout << "\tExport functions: " << endl;
+	cout << "\t\tEntryPoint\tOrdinal\tName" << endl;
+
+	//Print all export functions data
+	for (int i = 0; i < ptrImageExportDirectory->NumberOfFunctions; i++)
+	{
+		if (ptrExportsFunctions[i] == 0)
+			continue;
+		cout << "\t\t" << ptrExportsFunctions[i] << '\t' << i + ptrImageExportDirectory->Base << '\t';
+
+		//Print name if needed
+		for (DWORD j = 0; j < ptrImageExportDirectory->NumberOfNames; j++)
+		{
+			//Check if needed
+			if (ptrExportsNameOrdinals[j] == i)
+				cout << (char*)(imageBase + ptrExportsNames[j]);
+		}
+
+		//Check if the function is forwarded functions(address inside export RVA) and print if so
+		if ((ptrExportsFunctions[i] >= exportStartRVA) && (ptrExportsFunctions[i] <= exportEndRVA))
+			cout << "[Forwarded function -> " << imageBase + ptrExportsFunctions[i] << "]";
+		cout << endl;
+	}
 }
 
 /// <summary>Print the meta data of PE file</summary>
@@ -77,8 +124,32 @@ void printPEMetaData(DWORD imageBase)
 PIMAGE_SECTION_HEADER getSectionHeaderByRVA(DWORD RVA, PIMAGE_NT_HEADERS ptrImageNTHeader)
 {
 	//Get pointer to the first section
-	PIMAGE_SECTION_HEADER section = IMAGE_FIRST_SECTION
+	PIMAGE_SECTION_HEADER section = IMAGE_FIRST_SECTION(ptrImageNTHeader);
 
+	for (int i = 0; i < ptrImageNTHeader->FileHeader.NumberOfSections; i++, section++)
+	{
+		//Check if RVA inside the section range and return this section if so
+		if ((RVA >= section->VirtualAddress) && (section->VirtualAddress + section->Misc.VirtualSize))
+			return section;
+	}
+	return 0;
+}
+
+DWORD getDataDirectoryPtr(DWORD imageBase, int dataDirectoryIndex)
+{
+	// Get the headers of the PE
+	PIMAGE_DOS_HEADER ptrDosHeader = (PIMAGE_DOS_HEADER)imageBase;
+	PIMAGE_NT_HEADERS32 ptrNtHeaders = (PIMAGE_NT_HEADERS32)(ptrDosHeader->e_lfanew + imageBase);
+	PIMAGE_OPTIONAL_HEADER32 ptrOptionalHeader = (PIMAGE_OPTIONAL_HEADER32)& ptrNtHeaders->OptionalHeader;
+
+	// If the data directory index is false, then the function returns the optional header virtual address.
+	if (!dataDirectoryIndex) {
+		return (DWORD)ptrOptionalHeader;
+	}
+
+	// Get the vitual address of the requested data directory and return it.
+	PIMAGE_DATA_DIRECTORY ptrDataDirectory = &ptrOptionalHeader->DataDirectory[dataDirectoryIndex];
+	return (DWORD)ptrDataDirectory;
 }
 
 void printPEImports(DWORD imageBase)
@@ -97,7 +168,7 @@ void printPEImports(DWORD imageBase)
 	}
 
 	// Get the first import descriptor
-	PIMAGE_DATA_DIRECTORY ptrImportsDataDirectory = (PIMAGE_DATA_DIRECTORY)GetDataDirectoryPtr(imageBase, IMAGE_DIRECTORY_ENTRY_IMPORT);
+	PIMAGE_DATA_DIRECTORY ptrImportsDataDirectory = (PIMAGE_DATA_DIRECTORY)getDataDirectoryPtr(imageBase, IMAGE_DIRECTORY_ENTRY_IMPORT);
 	PIMAGE_IMPORT_DESCRIPTOR ptrImportDescriptor = (PIMAGE_IMPORT_DESCRIPTOR)(ptrImportsDataDirectory->VirtualAddress + imageBase);
 
 	cout << "Imports: " << endl;
@@ -136,6 +207,33 @@ void printPEImports(DWORD imageBase)
 	}
 	return;
 }
+
+void printPEExports(DWORD imageBase)
+{
+	// Get the export section.
+	PIMAGE_DOS_HEADER ptrDosHeader = (PIMAGE_DOS_HEADER)imageBase;
+	PIMAGE_NT_HEADERS32 ptrNtHeaders = (PIMAGE_NT_HEADERS32)(ptrDosHeader->e_lfanew + imageBase);
+	//Get export
+	DWORD exportsStartRVA = ptrNtHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress;
+	DWORD exportsEndRVA = exportsStartRVA + ptrNtHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].Size;
+	PIMAGE_SECTION_HEADER ptrExportsHeader = getSectionHeaderByRVA(exportsStartRVA, ptrNtHeaders);
+
+	//Check that the header isnt empty
+	if (!ptrExportsHeader)
+	{
+		cout << "No exports" << endl;
+		return;
+	}
+
+	//Get export dir pointer
+	PIMAGE_EXPORT_DIRECTORY ptrImageExportDirectory = (PIMAGE_EXPORT_DIRECTORY)(imageBase + exportsStartRVA);
+
+	//Print export metadata
+	printExportMetaData(ptrImageExportDirectory);
+	printExportFunctionsData(imageBase, ptrImageExportDirectory, exportsStartRVA, exportsEndRVA);
+
+}
+
 int main(int argc, char** args)
 {
 	//check that we have path
@@ -154,7 +252,7 @@ int main(int argc, char** args)
 	cout << "-------------------Exploring imports-------------------" << endl;
 	//printPEImports(imageBase);
 	cout << "-------------------Exploring Exports-------------------" << endl;
-	//printPEExports(imageBase);
+	printPEExports(imageBase);
 	cout << "Done!" << endl;
 	return 0;
 }
